8e261aa129819fae7c0254c4de105c2cf474cd59
Juel Valdivia <jooelvaldivi@gmail.com>
2023-06-27 13:34:24 -0600

    chore(pipelines): Add github actions

--- src/core/socialNetwork/errors.ts
+++ src/core/socialNetwork/errors.ts
@@ -1,6 +1,6 @@
 export class SocialNetworksError extends Error {
-  constructor(message?: string) {
-    super(message);
+  constructor(message?: string, error?: Error) {
+    super(message, error);
 
     Object.setPrototypeOf(this, SocialNetworksError.prototype);
     this.name = 'SocialNetworksError';
