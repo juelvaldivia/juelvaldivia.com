6767a168c27c9658764c4574114d328c157c9153
Juel Valdivia <jooelvaldivi@gmail.com>
2023-07-07 15:39:52 -0600

    feat(background): Remove particles effect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,11 @@ import Welcome from '../components/Welcome';
 import About from '../components/About';
 import Social from '../components/Social';
 import BuyMeACoffee from '../components/BuyMeACoffee';
-import ParticlesBackground from '../components/ParticlesBackground';
 import { Container } from '../styles';
 
 const Home = () => {
   return (
     <>
-      <ParticlesBackground />
       <Container>
         <Welcome />
         <BuyMeACoffee />
