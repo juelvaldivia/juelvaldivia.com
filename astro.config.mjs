99622e9ba328f4c2de75d545b72b847e30772bbc
Juel Valdivia <jooelvaldivi@gmail.com>
2023-11-06 22:54:30 -0600

    chore: add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://juelvaldivia.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  trailingSlash: 'always', // Use to always append '/' at end of url
+  site: 'https://juelvaldivia.com/',
+  // Use to generate your sitemap and canonical URLs in your final build.
+  trailingSlash: 'always',
+  // Use to always append '/' at end of url
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'monokai',
-    },
+      theme: 'monokai'
+    }
   },
   integrations: [react(), tailwind({}), sitemap(), robotsTxt()],
-})
+  output: "server",
+  adapter: netlify()
+});
